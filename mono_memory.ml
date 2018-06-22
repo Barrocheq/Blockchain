9453d1c5ff15c8c03dcaafe4679d227367bb1f2b
twans <antoine.dolant@u-psud.fr>
2018-06-22 08:58:41 +0200

    added random nonce

--- mono_memory.ml
+++ mono_memory.ml
@@ -15,17 +15,18 @@ open Types
 open Helpers
 
 let main () =
+	Random.self_init;
 	let first_block = get_genesis in
 		let bc = empty_blockchain first_block in 
 
 		let rec create_blockchain l =
 			match l with 
-			| previous::c -> let content = mk_block_content "isd_groupe_4" [] previous 0 20 in 
+			| previous::c -> let content = mk_block_content "isd_groupe_4" [] previous (Random.bits ()) 20 in 
 							 let info = mk_block_info (previous.block_info.b_level + 1) (hash_string (block_content_to_string content)) in
 							 let block = {block_info = info; block_ctt = content} in 
 							 print_string (block_content_to_string block.block_ctt); 
 							 create_blockchain (block::c)
-			| _ -> let content = mk_block_content "isd_groupe_4" [] first_block.g_block 0 20 in 
+			| _ -> let content = mk_block_content "isd_groupe_4" [] first_block.g_block (Random.bits ()) 20 in 
 				   let info = mk_block_info (first_block.g_block.block_info.b_level + 1) (hash_string (block_content_to_string content)) in
 				   let block = {block_info = info; block_ctt = content} in 
 				   		print_string (block_content_to_string block.block_ctt);
